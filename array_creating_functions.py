23fa7c81f3f66807467ba5e23e6387a26e915f42
Ma-a-sc <mark.scharmann.ms@gmail.com>
2021-03-29 11:34:49 +0200

    v0.1

--- array_creating_functions.py
+++ array_creating_functions.py
@@ -8,16 +8,19 @@ def create_variable(var_type, num_of_var):
     if var_type == "int":
         name = input("V_name")
 
+        bot = int(input("Minimum"))
+
+        top = int(input("Maximum"))
+
         v_name =[name]
 
         how_many_var = num_of_var
 
         indexes = af.index_setter(how_many_var) 
 
-        ## still need to write a function that fills a defined array with 
-        ## random integer. At the moment it is not in but float in a range of
-        ## -2 to 2.
-        int_array = pd.DataFrame(np.random.randn(how_many_var,1), index=indexes, 
+        list_for_array = af.fill_int_aray(bot, top, num_of_var)
+        
+        int_array = pd.DataFrame(list_for_array, index=indexes, 
         columns=(v_name))
 
         print(int_array)
@@ -25,20 +28,46 @@ def create_variable(var_type, num_of_var):
     elif var_type == "float":
         name = input("V_name")
 
+        bot = int(input("Minimum"))
+
+        top = int(input("Maximum"))
+
         v_name =[name]
 
         how_many_var = num_of_var
 
         indexes = af.index_setter(how_many_var) 
 
-        int_array = pd.DataFrame(np.random.randn(how_many_var,1), index=indexes, 
+        list_for_array = af.fill_float_array(bot, top, num_of_var)
+
+        float_array = pd.DataFrame(list_for_array, index=indexes, 
         columns=(v_name))
 
-        print(int_array)
+        print(float_array)
 
 
     elif var_type == "dummy":
 
+        name = input("V_name")
+
+        bot = 0
+
+        top = 1
+
+        v_name =[name]
+
+        how_many_var = num_of_var
+
+        indexes = af.index_setter(how_many_var) 
+
+        list_for_array = af.fill_int_aray(bot, top, num_of_var)
+
+        dummy_array = pd.DataFrame(list_for_array, index=indexes, 
+        columns=(v_name))
+
+        print(dummy_array)
+
+    elif var_type == "string":
         name = input("V_name")
 
         v_name =[name]
@@ -47,17 +76,32 @@ def create_variable(var_type, num_of_var):
 
         indexes = af.index_setter(how_many_var)
 
+        ## A function is needed that fills the array randomly with the possibilities
+        ## given by the user
 
-
-        int_array = pd.DataFrame(np.random.randn(how_many_var,1), index=indexes, columns=(v_name))
+        int_array = pd.DataFrame(np.random.randn(how_many_var,1), index=indexes, 
+        columns=(v_name))
 
         print(int_array)
-
-    elif var_type == "string":
 
         string_array = np.array([])
 
     elif var_type == "boolean":
+        name = input("V_name")
+
+        v_name =[name]
+
+        how_many_var = num_of_var
+
+        indexes = af.index_setter(how_many_var)
+
+        ## Function mussing that fills the array with true and false. Is more or
+        ## less the same as the dummy variable function.
+
+        int_array = pd.DataFrame(np.random.randn(how_many_var,1), index=indexes, 
+        columns=(v_name))
+
+        print(int_array)
 
         boolean_array = np.array([])
 
@@ -73,4 +117,4 @@ class descriptive_statistics():
     def __init__(self):
         pass
 
-create_variable("int", 70)
+create_variable("float", 70)
--- array_functions.py
+++ array_functions.py
@@ -1,12 +1,40 @@
+import random
+
 def index_setter(int_input):
-    x = 0
-    y = 0
+    x = 1
+    y = 1
     indexes = []
     
-
-    while x < int_input:
+    while x <= int_input:
         indexes.append(y)
         x  += 1
         y += 1
 
     return indexes
+
+
+def fill_int_aray(bot, top, number_of_obs):
+    x = 0
+
+    array = []
+
+    while x < number_of_obs:
+        y = random.randint(bot, top)
+        array.append(y)
+
+        x += 1
+
+    return array
+
+def fill_float_array(bot, top, number_of_obs):
+    x = 0
+
+    array = []
+
+    while x < number_of_obs:
+        y = random.uniform(bot, top)
+        array.append(y)
+
+        x += 1
+
+    return array
