a40743aea6da3d4e1ff6f65dd4a65e2030064fa1
Ma-a-sc <mark.scharmann.ms@gmail.com>
2021-03-31 23:49:12 +0200

    v0.1

---
+++ descriptive_statistics_functions.py
@@ -0,0 +1,19 @@
+class descriptive_statistics(s_type, array):
+
+    def _init__(s_type, array):
+        if self.s_type == "mean":
+            descriptive_statistics.mean(self.array)
+
+
+    
+    def mean():
+
+    def median():
+
+    def v_min():
+    
+    def v_max():
+    
+    def mode():
+
+    def
